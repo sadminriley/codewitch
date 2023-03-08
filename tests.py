c48865f05e88a6b549eec4d35c84d28f989e13c3
Riley <rileymace57@gmail.com>
2023-03-08 18:38:28 +0000

    Add / test for flask route for CircleCI

---
+++ tests.py
@@ -0,0 +1,14 @@
+import sys
+
+
+# this is not pythonic and hacky, fix this later!
+sys.path.append('lib')
+from flask_upload import app
+
+
+
+
+def test_index_route():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
